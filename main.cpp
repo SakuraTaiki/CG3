25079b59ac53669a185f5ce1da34a229900b733d
SakuraTaiki <k024g0010@g.neec.ac.jp>
2025-04-16 15:03:07 +0900

    ウィンドウを表示

--- main.cpp
+++ main.cpp
@@ -1,24 +1,81 @@
 #include <Windows.h>
+#include <cstdint>　　	//ウィンドウを作ろう6ページから
+
+
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		//ウィンドウが破壊された
+	case WM_DESTROY:
+		//OSに対してアプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
 
 //Windousアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
 {
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名	
+	wc.lpszClassName = L"CG2WIndowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+
+	//クライエント領域のサイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeith = 720;
+	//ウィンドウサイズを表す構造体にクライエント領域を入れる
+	RECT wrc = { 0,0,kClientWidth,kClientHeith };
+	//クライエント領域を元に実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(wc.lpszClassName,//使用するクラス名
+		L"CG2",//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,//よく見るウィンドウスタイル
+		CW_USEDEFAULT,//X座標
+		CW_USEDEFAULT,//Y座標
+		wrc.right - wrc.left,//横幅
+		wrc.bottom - wrc.top,//縦幅
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr);
+
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+
+	while (msg.message != WM_QUIT) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else {
+
+		}
+
+	}
 	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
-	
+
 	return 0;
 }
-////ウィンドウプロシージャ
-//LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-//	//メッセージに応じてゲーム固有の処理を行う
-//	switch (msg)
-//	{
-//		//ウィンドウが破壊された
-//	case WM_DESTROY:
-//	//OSに対してアプリの終了を伝える
-//		PostQuitMessage(0);
-//		return 0;
-//	}
-//	//標準のメッセージ処理を行う
-//	return DefWindowProc(hwnd, msg, wparam, lparam);
-//}
+	
+	
+	
+
+
+
