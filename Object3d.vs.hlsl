88c04140ffffd127ada713d521ce837ae28b2ad7
SakuraTaiki <k024g0010@g.neec.ac.jp>
2025-11-26 13:46:05 +0900

    実行は通ったが10枚を同時に描画ができず1枚のみ描画されてしまっている

--- Object3d.vs.hlsl
+++ Object3d.vs.hlsl
@@ -1,9 +1,5 @@
 #include"Object3d.hlsli"
-struct TransformationMatrix
-{
-    float32_t4x4 WVP;
-    float32_t4x4 World;
-};
+
 ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 
