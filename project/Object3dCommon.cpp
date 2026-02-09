9858fedd7da0d72f4cc0f512f906f155aa734237
SakuraTaiki <k024g0010@g.neec.ac.jp>
2026-02-09 14:51:51 +0900

    実行確認完了

--- project/Object3dCommon.cpp
+++ project/Object3dCommon.cpp
@@ -1,1 +1,30 @@
 #include "Object3dCommon.h"
+#include"DirectXCommon.h"
+#include"WinApp.h"
+#include"dxcapi.h"
+#include<cassert>
+
+using namespace Microsoft::WRL;
+
+void Object3dCommon::SetCommonDrawSettings(ID3D12GraphicsCommandList* commandList) {
+
+	// 1. ルートシグネチャをセットするコマンド
+	commandList->SetGraphicsRootSignature(rootSignature_.Get());
+
+	// 2. グラフィックスパイプラインステートをセットするコマンド
+	commandList->SetPipelineState(graphicsPipelineState_.Get());
+
+	// 3. プリミティブトポロジーをセットするコマンド
+	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+}
+
+
+void Object3dCommon::Initialize(DirectXCommon* dxCommon)
+{
+	dxCommon_ = dxCommon;
+
+	assert(dxCommon_ != nullptr);
+
+	// グラフィックスパイプラインの生成を呼び出す
+	CreateGraphicsPipelineState();
+}
