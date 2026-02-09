4e732b9d63ba2e3e2ea34038a3b261366b0cdf9b
SakuraTaiki <k024g0010@g.neec.ac.jp>
2026-02-09 14:37:10 +0900

    ObjectCommon途中まで

--- project/Object3dCommon.h
+++ project/Object3dCommon.h
@@ -1,7 +1,21 @@
 #pragma once
+#include "DirectXCommon.h"
+#include <d3d12.h>
+#include <wrl.h>
 class Object3dCommon
 {
 public:
-	void Initialize();
+	void Initialize(DirectXCommon* dxCommon);
+
+	ID3D12RootSignature* GetRootSignature() const { return rootSignature_.Get(); }
+
+	ID3D12PipelineState* GetPipelineState() const { return graphicsPipelineState_.Get(); }
+
+private:
+	void CreateRootSignature();
+	void CreateGraphicsPipelineState();
+	DirectXCommon* dxCommon_;
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_;
 };
 
