3f361749c7c8f14d3f9e71bd76c6a049c67c349b
SakuraTaiki <k024g0010@g.neec.ac.jp>
2026-04-16 13:53:28 +0900

    色々変更

--- project/Sprite.PS.hlsl
+++ project/Sprite.PS.hlsl
@@ -8,11 +8,7 @@ struct Material
 {
     float4 color;
 };
-
-cbuffer Material : register(b0)
-{
-    float4 color;
-};
+ConstantBuffer<Material> gMaterial : register(b0);
 
 Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
