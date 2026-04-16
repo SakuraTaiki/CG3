331bc6ad41903aee8ba99f28c9e36b5767e63941
SakuraTaiki <k024g0010@g.neec.ac.jp>
2026-04-16 14:25:14 +0900

    複数モデルを同時に描画できた

--- project/Sprite.VS.hlsl
+++ project/Sprite.VS.hlsl
@@ -1,26 +1,30 @@
-struct TransformationMatrix
+// 定数バッファ（VS : b0）
+cbuffer TransformationMatrix : register(b0)
 {
     float4x4 WVP;
 };
-ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
+// 入力
 struct VertexShaderInput
-    
 {
     float4 position : POSITION0;
     float2 texcoord : TEXCOORD0;
 };
 
+// 出力（PSへ渡す）
 struct VertexShaderOutput
 {
     float4 position : SV_POSITION;
     float2 texcoord : TEXCOORD0;
 };
 
+// メイン
 VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
-    output.position = mul(input.position, gTransformationMatrix.WVP);
+
+    output.position = mul(input.position, WVP);
     output.texcoord = input.texcoord;
+
     return output;
 }
