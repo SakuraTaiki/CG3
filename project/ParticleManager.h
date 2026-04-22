8d8428bf74ff288b9da93616a83d96c92fe20fd1
SakuraTaiki <k024g0010@g.neec.ac.jp>
2026-04-22 12:59:10 +0900

    hを変更

--- project/ParticleManager.h
+++ project/ParticleManager.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <d3d12.h>
+#include <wrl.h>
+#include <vector>
+#include <list>
+#include "MyMath.h"
+#include "DirectXCommon.h"
+#include "TextureManager.h"
+
+struct particle
+{
+	Transform transform;
+	Vector3 velocity;
+	Vector4 color;
+	float lifeTime;
+	float maxTime;
+};
+
 class ParticleManager
 {
+public:
+
+	void Initialize();
+	void Emit(const std::string name, const Vector3& position, uint32_t count);
+
 };
 
